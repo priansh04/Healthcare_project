26fc637b8bc62b2cc0d8887be4b28de5015f3577
Prianshu <your.email@example.com>
2024-10-21 14:36:53 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Import express
+const express = require('express');
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middlewares/errorHandler");
+const cors = require("cors");
 
+connectDb();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+app.use(cors());
+
+// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello, World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Start the server on port 3000
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
